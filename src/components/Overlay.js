5b9a8cb9e04eed0e1276bd7afa992edf78fe4979
Uddekudde <33427362+Uddekudde@users.noreply.github.com>
2018-10-05 01:25:57 +0200

    updated overlay text

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -51,16 +51,17 @@ export default class Overlay extends React.Component {
                                 <h1 className="title1 animated fadeInUp" style={{animationDelay: ".5s"}} >Hello,</h1>
                                 <h2 className="title2 animated fadeInUp" style={{animationDelay: ".7s"}} >Is it me you're looking for?</h2>
                                 <p className="description animated fadeInUp" style={{animationDelay: ".9s"}} >
-                                    I am currently studying software engineering at KTH Kista. <br/>
-                                    My interests are UX and web development, but I have knowledge in many areas in IT. <br/>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
-                                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  <br/>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
+                                    I am currently finishing my studies in software engineering at KTH Kista. <br/>
+                                    My main interests are UX and web app development, but I have knowledge in many areas in IT. <br/>
+                                    Among my individual courses were human computer interaction, mobile application development and ethical hacking. <br/>
+                                </p>
+                                <p className="description animated fadeInUp" style={{animationDelay: ".9s"}} >
+                                    In my free time I do digital painting and animation in Photoshop. <br/>
+                                    I am always looking for new technologies to learn. <br/>
                                 </p>
                                 <p className="description animated fadeInUp" style={{animationDelay: ".9s"}} >
                                     You can reach me at tudd@kth.se <br/>
-                                    other links: Github LinkedIn <br/>
+                                    <a style={{color:'white'}} href='https://github.com/Uddekudde'>GitHub</a> LinkedIn <br/>
                                 </p>
                                 <div style={{height:'3em'}}/>
                             </div>
